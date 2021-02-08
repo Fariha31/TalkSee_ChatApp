9f364b10c7012972655b955e1c1958261e196f83
Fariha31 <62641238+Fariha31@users.noreply.github.com>
2021-02-08 15:08:26 +0500

    1st commit

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./Header";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import notFound from "./notFound";
+const App = () => {
+  return (
+    
+      <BrowserRouter>
+        <Header />
+         
+        <Switch>
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/notfound" component={notFound} />
+          <Redirect to="/notfound" />
+        </Switch>
+      </BrowserRouter>
+   
+  );
+};
+
+export default App;
---
+++ client/src/components/userDashboard.js
@@ -0,0 +1,6 @@
+import React from "react";
+const userDashboard = () => {
+  return <div><h1> Hi I am user</h1></div>;
+};
+
+export default userDashboard;
